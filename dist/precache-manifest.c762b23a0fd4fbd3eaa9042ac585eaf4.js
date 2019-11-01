dd69edd09d787fec153fe9d8860eab5fee8f1b25
mohammadhosseinfaghihi <Mohammed.Faghihi@tecnotree.com>
2019-11-02 01:54:19 +0330

    add puncher

---
+++ dist/precache-manifest.c762b23a0fd4fbd3eaa9042ac585eaf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d75b32058fbb822609ae8b6fb95030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19465041fa888b08899b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "57e746b9fd7e8bdf6d46",
+    "url": "/static/js/2.265816b2.chunk.js"
+  },
+  {
+    "revision": "19465041fa888b08899b",
+    "url": "/static/js/main.8f9a856f.chunk.js"
+  },
+  {
+    "revision": "3a7aa687ff3bc69aeeff",
+    "url": "/static/js/runtime-main.46b2df3b.js"
+  }
+]);
